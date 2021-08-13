15a6e7773db37c8412216725f167a8c12af1df8e
Mirza Smrkovic <mirza.smrkovic@gmail.com>
2021-08-13 17:52:50 +0200

    add PORT env

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 import { openIntercom, closeDoor, openDoor, authorize } from './controllers'
 
-const port = 3000
+const port = process.env.PORT || 3000
 const app = express()
 
 // middleware
