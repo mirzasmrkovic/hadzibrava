784747458cd89372d3d834820d3458e097c705fe
Mirza Smrkovic <mirza.smrkovic@gmail.com>
2021-08-20 18:19:27 +0200

    serve build of react

--- server/server.js
+++ server/server.js
@@ -4,13 +4,13 @@ import path from 'path'
 
 import { openIntercom, closeDoor, openDoor, authorize } from './controllers'
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 const app = express()
 
 // middleware
 app.disable('x-powered-by')
 // serving static html
-app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, '../brava-fe/build')))
 
 // Auth
 app.post('/auth', authorize)
