ed1989620d84fac4465010d40949aaca4f2368c1
Mirza Smrkovic <mirza.smrkovic@gmail.com>
2021-08-25 15:47:31 +0200

    remove cors whitelist

--- server/server.js
+++ server/server.js
@@ -6,14 +6,6 @@ import { openIntercom, closeDoor, openDoor, authorize } from './controllers'
 
 const port = process.env.PORT || 4000
 const app = express()
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, psw'
-  )
-  next()
-})
 
 // middleware
 app.disable('x-powered-by')
