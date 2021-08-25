d829779620ccadff93da72cee5078f3a7168d42f
Mirza Smrkovic <mirza.smrkovic@gmail.com>
2021-08-25 13:42:39 +0200

    add router rendering

--- brava-fe/src/App.js
+++ brava-fe/src/App.js
@@ -1,11 +1,20 @@
 import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Auth from './components/Auth.js'
+import Buttons from './components/Buttons.js'
 
 const App = () => {
   return (
-    <div>
-      <Auth />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Auth />
+        </Route>
+        <Route path="/buttons">
+          <Buttons />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
