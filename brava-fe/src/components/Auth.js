bfdbc1fda5d4a238ebf7f5988b9e7ed6617b79d2
Mirza Smrkovic <mirza.smrkovic@gmail.com>
2021-08-25 13:39:38 +0200

    move authentication logic to parent

--- brava-fe/src/components/Auth.js
+++ brava-fe/src/components/Auth.js
@@ -1,21 +1,48 @@
 import React, { useState } from 'react'
 import Input from './Input'
 import InputDisplay from './InputDisplay'
+import axios from 'axios'
+import { useHistory } from 'react-router-dom'
 
 const Auth = () => {
   const [val, setValue] = useState('')
+  const history = useHistory()
+  const APIPath = process.env.REACT_APP_API || 'http://localhost:4000/'
+  const handleSubmit = (currentVal, setValue) => {
+    const endpoint = 'auth'
+    axios({
+      method: 'post',
+      url: endpoint,
+      baseURL: APIPath,
+      headers: { psw: currentVal },
+    })
+      .then(res => {
+        history.push({
+          pathname: '/buttons',
+          state: {
+            buttons: res.data.buttons,
+            password: currentVal,
+          },
+        })
+      })
+      .catch(e => {
+        setValue('')
+        console.error(e)
+      })
+  }
   return (
     <form
       id="password-form"
       onSubmit={e => {
         e.preventDefault()
+        setValue('')
       }}
     >
       <span id="psw-title">Upisite hadzi-sifru</span>
       <span id="psw-status"></span>
       <div id="display-wrapper">
         <InputDisplay currentLength={val.length - 1} />
-        <Input val={val} setValue={setValue} />
+        <Input val={val} setValue={setValue} handleSubmit={handleSubmit} />
       </div>
     </form>
   )
--- brava-fe/src/components/Input.js
+++ brava-fe/src/components/Input.js
@@ -1,29 +1,6 @@
 import React from 'react'
-import axios from 'axios'
-import HIButton from './Buttons'
 
-const Input = ({ val, setValue }) => {
-  const APIPath = process.env.REACT_APP_API || 'http://localhost:4000/'
-  const handleSubmit = (currentVal, setValue) => {
-    axios.defaults.headers.post['psw'] = currentVal
-    const endpoint = 'auth'
-    axios
-      .post(APIPath + endpoint)
-      .then(res => {
-        // Remove the password input on correct psw input
-        // const passwordForm = document.getElementById('password-form')
-        // passwordForm.parentNode.removeChild(passwordForm)
-
-        // Show buttons after removing
-        const buttons = res.data.buttons
-        for (let i = 0; i < buttons.length; i++) <HIButton />
-      })
-      .catch(e => {
-        setValue('')
-        console.error(e)
-      })
-  }
-
+const Input = ({ val, setValue, handleSubmit }) => {
   return (
     <input
       value={val}
